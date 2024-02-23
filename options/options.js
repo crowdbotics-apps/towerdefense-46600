e1b22150ef8965c720c1a66baa15f168069b9a11
Crowdbotics <team@crowdbotics.com>
2024-02-23 08:53:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "towerdefense-46600",
+  url: "https://towerdefense-46600.botics.co",
+  api: "https://towerdefense-46600.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
